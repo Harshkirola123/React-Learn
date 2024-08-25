8f739396645d54457656b69263cee2a6feecb977
Harshit <harshitkirola123@gmail.com>
2024-08-25 18:18:21 +0530

    add basic react project

---
+++ basic-react/src/hello.jsx
@@ -0,0 +1,12 @@
+function Hello(){
+    return(
+        <h3>Hello World</h3>
+    );
+}
+
+export default Hello;
+function print(){
+    return(
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus quos temporibus debitis eligendi, iste doloribus beatae molestiae sapiente voluptas itaque fugit nisi iure sint, cupiditate enim, repudiandae repellat exercitationem aut.</p>
+    );
+}
